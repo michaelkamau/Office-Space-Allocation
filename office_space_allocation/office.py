1a3036386384f386a645efb377c3d9243729e632
Michael Kamau <tokamau@gmail.com>
2016-11-07 12:45:31 +0300

    Refactor: Office subclasses Room

--- office_space_allocation/office.py
+++ office_space_allocation/office.py
@@ -1,3 +1,6 @@
-class Office:
-    #TODO: complete definition of this class
+from office_space_allocation.room import Room
+
+
+class Office(Room):
+    # TODO: complete definition of this class
     pass
