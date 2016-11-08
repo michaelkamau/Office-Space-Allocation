03b046b484c072d6b80c245662ff4357ba9f2433
Michael Kamau <tokamau@gmail.com>
2016-11-08 15:03:29 +0300

    Add method to add Persons to list of all_persons

--- office_space_allocation/amity.py
+++ office_space_allocation/amity.py
@@ -1,3 +1,14 @@
+from office_space_allocation.person import Person
+
+
 class Amity:
     # TODO : Complete definition
-    pass
+    def __init__(self):
+        self.all_persons = []
+
+    def add_person(self, person):
+        """
+        Adds a new Person to list of persons
+        :param: ```Person``` person
+        """
+        self.all_persons.append(person)
