0ec90a474fa5b4fd52e086f00f6a3fc72dc44e58
Michael Kamau <tokamau@gmail.com>
2016-11-08 11:43:02 +0300

    Add method to add Person to Livingroom room

--- office_space_allocation/livingroom.py
+++ office_space_allocation/livingroom.py
@@ -1,7 +1,18 @@
 from office_space_allocation.room import Room
+from office_space_allocation.fellow import Fellow
 
 
 class LivingRoom(Room):
+    def add_person(self, person):
+        """
+        Adds ```Person``` person to the list of occupants of the LivingRoom
+        Person should only be of type Fello
+
+        :param : ```Person``` person
+        """
+        if isinstance(person, Fellow):
+            self.occupants.append(person)
+
     def can_accept_occupants(self):
         return self.get_occupants() <= 4
 
