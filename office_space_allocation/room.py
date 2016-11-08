467213fa29b8fac40ac732516d2b1e5e3d727287
Michael Kamau <tokamau@gmail.com>
2016-11-08 10:31:16 +0300

    Add method to fetch number of occupant in room

--- office_space_allocation/room.py
+++ office_space_allocation/room.py
@@ -1,4 +1,12 @@
 class Room:
-    #TODO: complete class definition
+    # TODO: complete class definition
     def __init__(self, name):
         self.name = name
+        self.occupants = []
+
+    def get_occupants(self):
+        """
+        Fetches the number of occupants in the room
+        :return: ```int``` : occupants
+        """
+        return len(self.occupants)
