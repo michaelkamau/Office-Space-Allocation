8481a7ab685fbcfdde2b2cf0fc1d0b97b61033ad
Michael Kamau <tokamau@gmail.com>
2016-11-09 05:55:10 +0300

    Add method to allocate room randomly

--- office_space_allocation/amity.py
+++ office_space_allocation/amity.py
@@ -1,4 +1,8 @@
 from office_space_allocation.person import Person
+from office_space_allocation.fellow import Fellow
+from office_space_allocation.staff import Staff
+from office_space_allocation.livingroom import LivingRoom
+import random
 
 
 class Amity:
@@ -6,6 +10,7 @@ class Amity:
     def __init__(self):
         self.all_persons = []
         self.all_rooms = []
+        self.allocated_rooms = []
 
     def add_room(self, new_room):
         """
@@ -35,3 +40,33 @@ class Amity:
             if name == r.get_name().lower():
                 return r
         raise ValueError(name + " not found!")
+
+    def allocate_room(self, person):
+        """
+        Allocates a room to ```Person``` person.
+
+        This is done randomly.
+        :param person: ```Person``` to allocate room
+        :return: room : ```Room``` allocated , if possible
+        """
+        if isinstance(person, Fellow):
+            # raise exception if there are no rooms
+            if len(self.all_rooms) < 1:
+                raise IndexError("There are no available rooms")
+            else:
+                rm = random.choice(self.all_rooms)
+                rm.add_person(person)
+                self.allocated_rooms.append(rm)
+                return rm
+        elif isinstance(person, Staff):
+            # allocate only livingrooms
+            livingrooms = [lr for lr in self.all_rooms if isinstance(lr, LivingRoom)]
+            if len(livingrooms) < 1:
+                raise IndexError("There are no more LivingRooms to allocate")
+            else:
+                rm = random.choice(livingrooms)
+                rm.add_person(person)
+                self.allocated_rooms.append(rm)
+                return rm
+        else:
+            raise TypeError("Person argument must be of type Staff or Fellow")
