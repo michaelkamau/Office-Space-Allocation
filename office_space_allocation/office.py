b200acbc43cb2910f947ec1a1241b953278af9a5
Michael Kamau <tokamau@gmail.com>
2016-11-09 15:13:45 +0300

    Refactor to raise exception if Room is full

--- office_space_allocation/office.py
+++ office_space_allocation/office.py
@@ -1,5 +1,6 @@
 from office_space_allocation.room import Room
 from office_space_allocation.fellow import Fellow
+from office_space_allocation.utilities import RoomFullError
 
 
 class Office(Room):
@@ -7,9 +8,14 @@ class Office(Room):
         """
         Adds Person ```person``` to the list of occupants of the Office room
         """
-        self.occupants.append(person)
+        # Check if room is full
+        if self.can_accept_occupants():
+            self.occupants.append(person)
+            print("Occupants: ", self.get_occupants())
+        else:
+            raise RoomFullError("Room is full!")
 
     def can_accept_occupants(self):
-        return self.get_occupants() <= 6
+        return self.get_occupants() < 7
 
 # TODO: complete definition of this class
