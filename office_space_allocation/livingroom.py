ef83226e63aa736d8b987a84af26b8739385e2c5
Michael Kamau <tokamau@gmail.com>
2016-11-09 15:14:51 +0300

    Refactor to raise exception of Room is full

--- office_space_allocation/livingroom.py
+++ office_space_allocation/livingroom.py
@@ -1,21 +1,24 @@
 from office_space_allocation.room import Room
 from office_space_allocation.fellow import Fellow
 from office_space_allocation.staff import Staff
-from office_space_allocation.utilities import InvalidRoomOccupantError
+from office_space_allocation.utilities import InvalidRoomOccupantError, RoomFullError
 
 
 class LivingRoom(Room):
     def add_person(self, person):
         """
         Adds ```Person``` person to the list of occupants of the LivingRoom
-        Person should only be of type Fello
+        Person should only be of type Fellow
 
         :param : ```Person``` person
         """
-        if isinstance(person, Fellow):
-            self.occupants.append(person)
-        elif isinstance(person, Staff):
-            raise InvalidRoomOccupantError("Staff cannot join LivingRooms")
+        if self.can_accept_occupants():
+            if isinstance(person, Fellow):
+                self.occupants.append(person)
+            elif isinstance(person, Staff):
+                raise InvalidRoomOccupantError("Staff cannot join LivingRooms")
+        else:
+            raise RoomFullError("Room is full!")
 
     def can_accept_occupants(self):
         return self.get_occupants() <= 4
