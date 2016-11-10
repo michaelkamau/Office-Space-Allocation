4e89f36a0a9d4ce99ca40ffab407b66e769c4f2a
Michael Kamau <tokamau@gmail.com>
2016-11-10 10:45:36 +0300

    Add method to fetch Person full name

--- office_space_allocation/person.py
+++ office_space_allocation/person.py
@@ -2,4 +2,14 @@ class Person:
     def __init__(self, fname, lname):
         self.first_name = fname
         self.last_name = lname
+
+    def get_full_name(self):
+        """
+        Returns the Person full name
+        Combination of first name and second name
+        :return: full name in title case
+        """
+        full_name = self.first_name.strip().title() + " " + self.last_name.strip().title()
+        return full_name
+
     # TODO: Complete definition here
